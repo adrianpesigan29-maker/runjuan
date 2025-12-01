5b56fb926a2ccf9cc9282952cabd5c794c7370b8
Adrian <adrianpesigan29@gmail.com>
2025-12-01 14:53:14 +0800

    Initial project setup with working index.php

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>RunJuan Development Environment</h1>";
+echo "<p>If you can see this message, your local development environment is working correctly.</p>";
+echo "<p>Current date and time: " . date('Y-m-d H:i:s') . "</p>";
+phpinfo();
+?>
